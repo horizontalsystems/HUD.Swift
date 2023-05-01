4511cb629700dad1a75f30c5043f5e197833ee70
Ermat <ealymbaev@gmail.com>
2023-05-01 14:47:00 +0600

    Update `ThemeKit` dependency to `2.0.0`

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ],
         dependencies: [
             .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-            .package(url: "https://github.com/horizontalsystems/ThemeKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/horizontalsystems/ThemeKit.Swift.git", .upToNextMajor(from: "2.0.0")),
             .package(url: "https://github.com/horizontalsystems/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
         ],
         targets: [
